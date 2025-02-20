85718128922143c69a63176c62714893abf941ba
Jerry Chen <chen88088@gmail.com>
2025-02-20 15:51:18 +0800

    add ArcgisProServer: preprocessing and postprocessing

---
+++ PreprocessingServer/config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# Server Manager Configuration
+SERVER_MANAGER_URL = "http://10.52.52.136:8000"
+
+# Machine Configuration
+MACHINE_ID = "machine_server_1"
+MACHINE_IP = "10.52.52.136"
+MACHINE_PORT = 8085
+MACHINE_CAPACITY = 2
+
+# MinIO Configuration
+MINIO_URL = "http://10.52.52.138:31000"
+MINIO_BUCKET = "testdvcfilemanagementfordag"
+MINIO_ACCESS_KEY = "testdvctominio"
+MINIO_SECRET_KEY = "testdvctominio"
+
+# Dataset Management Service Configuration
+DMS_SERVICE_URL = "http://192.168.158.43:8082"
